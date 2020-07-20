c535c0928f9dfcd5ff4b178bbe71f6c9813dbac9
Randy Wilson <contact@randywilson.io>
2020-07-20 19:22:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+add();
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const greeting = (first, last) => {
+  return `Hello ${first} ${last}, nice to meet you!`;
+};
+
+function multiplyByTen(num) {}
+
+function subtractFive(num) {}
+
+function areSameLength(str1, str2) {
+  // hint: look up .length
+}
+
+function areEqual(x, y) {}
+
+function lessThanNinety(num) {}
+
+function greaterThanFifty(num) {
+  //  look up javascript operators
+}
+
+function subtract(x, y) {}
+
+function getRemainder(x, y) {
+  //  look up javascript operators
+}
+
+function isEven(num) {
+  //  look up javascript operators
+}
